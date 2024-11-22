d3ead9e0e3b0fddb186ec93b461e2edde8d1b0f2
Francesco Frosolini <francesco.frosolini@edu.upct.es>
2024-11-22 11:13:53 +0100

    initial commit

---
+++ src/main/java/com/francesco/microservices/app/cursos/MicroserviciocursosApplication.java
@@ -0,0 +1,13 @@
+package com.francesco.microservices.app.cursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviciocursosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviciocursosApplication.class, args);
+    }
+
+}
