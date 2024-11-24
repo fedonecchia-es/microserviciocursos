37d9fc346c71ab2a1cca0c973378e00e05ec25cb
Francesco Frosolini <francesco.frosolini@edu.upct.es>
2024-11-24 19:29:20 +0100

    configured eureuka client

--- src/main/java/com/francesco/microservices/app/cursos/MicroserviciocursosApplication.java
+++ src/main/java/com/francesco/microservices/app/cursos/MicroserviciocursosApplication.java
@@ -3,6 +3,7 @@ package com.francesco.microservices.app.cursos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class MicroserviciocursosApplication {
 
